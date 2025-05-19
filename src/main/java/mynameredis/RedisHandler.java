795981d22790009e32d1243079a06b584f979f3b
MyName <myname0414@naver.com>
2025-05-19 02:25:09 -0700

    init

--- src/main/java/mynameredis/RedisHandler.java
+++ src/main/java/mynameredis/RedisHandler.java
@@ -1,2 +1,31 @@
-package mynameredis;public class RedisHandler {
+package mynameredis;
+
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisPubSub;
+import redis.clients.jedis.UnifiedJedis;
+
+public class RedisHandler {
+
+    protected UnifiedJedis publisherJedis;
+    protected UnifiedJedis subscriberJedis;
+
+    protected Thread subscriberThread;
+
+    public RedisHandler(String host, int port) {
+        HostAndPort node = new HostAndPort(host, port);
+        publisherJedis = new UnifiedJedis(node);
+        subscriberJedis = new UnifiedJedis(node);
+    }
+
+    public void subscribe(JedisPubSub subscriber, String[] channels) {
+        subscriberThread = new Thread(() -> {
+            subscriberJedis.subscribe(subscriber, channels);
+        }, "RedisSubThread");
+        subscriberThread.start();
+    }
+
+    public void sendMessage(String channle, String message) {
+        publisherJedis.publish(channle, message);
+    }
+
 }
