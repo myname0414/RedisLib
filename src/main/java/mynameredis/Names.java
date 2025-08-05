84ca284fd38de3b002e75acc84a15eb001ce7823
MyName <myname0414@naver.com>
2025-08-05 10:40:06 -0700

    init

---
+++ src/main/java/mynameredis/Names.java
@@ -0,0 +1,11 @@
+package mynameredis;
+
+public class Names {
+    public static String CHANNEL_LOBBY = "inferno_lobby";
+    public static String CHANNEL_GAME1 = "inferno_game1";
+    public static String TAG_CREATE_CHAR = "CREATECHAR";
+    public static String TAG_READY = "RDY";
+
+    public static String TAG_INIT_SAVE = "INIT_SAVE"; //proxy initiated
+    public static String TAG_SAVED = "SAVED"; // server initiated
+}
